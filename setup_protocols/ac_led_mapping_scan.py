ba6426d4096c1ce876869d500d7f05ad5cfda95e
cocov <victor.coco@cern.ch>
2017-04-13 13:42:06 +0200

    modif in the DAC settings for MC added ac_mapping_protocol

--- setup_protocols/ac_led_mapping_scan.py
+++ setup_protocols/ac_led_mapping_scan.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 from setup_fsm.fsm_steps import *
 from utils.logger import TqdmToLogger
 
-protocol_name = 'AC_LED_SCAN'
+protocol_name = 'AC_MAPPING_SCAN'
 
 def prepare_run(master_fsm):
     '''
@@ -18,7 +18,7 @@ def prepare_run(master_fsm):
 
     log = logging.getLogger(sys.modules['__main__'].__name__)
 
-    log.info('\033[1m\033[91m\t\t-|> Start the AC LED SCAN Protocol\033[0m')
+    log.info('\033[1m\033[91m\t\t-|> Start the %s Protocol\033[0m'%protocol_name)
 
     if not allocate(master_fsm): return False
     if not configure(master_fsm): return False
@@ -46,7 +46,7 @@ def end_run(master_fsm):
     if not deallocate(master_fsm): return False
 
     # ALLOCATE
-    log.info('\033[1m\033[91m\t\t-|> End the AC LED SCAN Protocol\033[0m')
+    log.info('\033[1m\033[91m\t\t-|> End the %s Protocol\033[0m'%protocol_name)
     return True
 
 
@@ -70,21 +70,34 @@ def run(master_fsm):
     # Turn on the AC LEDs
     master_fsm.elements['cts_core'].all_on('AC',0)
     # Get the AC LEDs level to run
-    AC_DAC_Levels = master_fsm.options['protocol_configuration']['levels']
+    ac_level = master_fsm.options['protocol_configuration']['ac_level']
 
     # Define the progress bar
-    log.info('\033[1m\033[91m\t\t-|> Start the AC DAC level loop\033[0m' )
-    pbar = tqdm(total=len(AC_DAC_Levels))
+    log.info('\033[1m\033[91m\t\t-|> Start the AC Mapping loop\033[0m' )
+    pixel_list = list(master_fsm.elements['cts_core'].pixel_to_led['AC'].keys())
+    pbar = tqdm(total=len(pixel_list))
     tqdm_out = TqdmToLogger(log, level=logging.INFO)
-    for i,level in enumerate(AC_DAC_Levels) :
-        for patch in master_fsm.elements['cts_core'].cts.LED_patches:
-            master_fsm.elements['cts_core'].cts_client.set_ac_level(patch.camera_patch_id, level)
 
-        log.debug('\033[1m\033[91m\t\t-|> Level%d\033[0m' % level)
-        timeout = float(master_fsm.options['protocol_configuration']['events_per_level'])\
-                  /master_fsm.options['generator_configuration']['rate']
-        timeout+=1.
-        if not run_level(master_fsm,timeout):
+    for patch in master_fsm.elements['cts_core'].LED_patches:
+        master_fsm.elements['cts_core'].set_ac_level(patch.camera_patch_id, level)
+    # loop over the pixels
+    pixel_list.sort()
+    for i, pix in enumerate(pixel_list):
+        if start_pixel and pix < start_pixel: continue
+        if stop_pixel and pix > stop_pixel: continue
+        if i > 0:
+            # turn off the previous pixel
+            master_fsm.elements['cts_core'].cts_client.set_led_status('AC', pixel_list[i - 1], False)
+            master_fsm.elements['cts_core'].ac_status[pixel_list[i - 1]] = 0
+            master_fsm.elements['cts_core'].ac_level[pixel_list[i - 1]] = 0
+        # turn on this pixel
+        master_fsm.elements['cts_core'].cts_client.set_led_status('AC', pixel_list[i], True)
+        master_fsm.elements['cts_core'].ac_status[pixel_list[i]] = 1
+        master_fsm.elements['cts_core'].ac_level[pixel_list[i]] = level
+        master_fsm.elements['cts_core'].generator.start_trigger_sequence()
+
+        log.debug('\033[1m\033[91m\t\t-|> Pixel%d\033[0m' % pix)
+        if not run_level(master_fsm,0.1):
             log.error('Failed at level %d'%level)
             return False
         pbar.update(1)
@@ -98,53 +111,3 @@ def run(master_fsm):
         return False
 
     return True
-
-
-
-    def loop_over_ac_pixels(self, level, timeout=0.05, n_triggers=10, frequency = 1000 , start_pixel = None, stop_pixel = None):
-        """
-        loop_over_ac_pixels(level,timeout,trigger,n_trigger)
-
-        A function to light AC pixels one after the other in increasing software pixel id
-
-        Inputs :
-                 - level   : the DAC level to apply (int)
-                 - timeout : the amount of time, in s, to keep the pixels on (float)
-                 - n_trigger : number of consecutive trigger for AC
-                 - start_pixel : the first pixel of the sequence
-                 - stop_pixel : the last pixel of the sequence
-        """
-        # Initialise the trigger
-        self.generator.configure_trigger(n_pulse = n_triggers, freq= frequency)
-        # loop over the patches to set the DAC level
-        for patch in self.cts.LED_patches:
-            self.cts_client.set_ac_level(patch.camera_patch_id, level)
-        # loop over the pixels
-        pixel_list = list(self.cts.pixel_to_led['AC'].keys())
-        pixel_list.sort()
-        for i, pix in enumerate(pixel_list):
-            if start_pixel and pix < start_pixel :continue
-            if stop_pixel and pix > stop_pixel :continue
-            if i > 0:
-                # turn off the previous pixel
-                self.cts_client.set_led_status('AC', pixel_list[i - 1], False)
-                self.ac_status[pixel_list[i - 1]] = 0
-                self.ac_level[pixel_list[i - 1]] = 0
-            time.sleep(timeout)
-            # turn on this pixel
-            self.cts_client.set_led_status('AC', pixel_list[i], True)
-            self.ac_status[pixel_list[i]] = 1
-            self.ac_level[pixel_list[i]] = level
-            self.generator.start_trigger_sequence()
-            if self.plotting:
-                self.plot()
-            time.sleep(timeout)
-        # turn off the last pixel
-        self.cts_client.set_led_status('AC', pixel_list[-1], False)
-        self.ac_status[pixel_list[-1]] = 0
-        # loop over hte patches and put the DAC back to 0
-        for patch in self.cts.LED_patches:
-            self.cts_client.set_ac_level(patch.camera_patch_id, 0)
-            self.ac_level[pixel_list[-1]] = level
-        # put back the default trigger configruation
-        self.generator.stop_trigger_sequence()
--- setup_protocols/load_mc_events.py
+++ setup_protocols/load_mc_events.py
@@ -20,7 +20,7 @@ def prepare_run(master_fsm):
 
     log = logging.getLogger(sys.modules['__main__'].__name__)
 
-    log.info('\033[1m\033[91m\t\t-|> Start the DC LED SCAN Protocol\033[0m')
+    log.info('\033[1m\033[91m\t\t-|> Start the %s Protocol\033[0m'%protocol_name)
 
     if not allocate(master_fsm): return False
     if not configure(master_fsm): return False
@@ -70,6 +70,7 @@ def load_led_parameters(master_fsm):
     param, covariance = ac_led_coefficient[:, :, 0], ac_led_coefficient[:, :, 2:7:1]
     return param
 
+'''
 def get_patch_DAC(pixels,pe_per_pixel,param):
     dac = 0
     total_pe = np.sum(pe_per_pixel)
@@ -80,7 +81,7 @@ def get_patch_DAC(pixels,pe_per_pixel,param):
     inv_p = lambda y: np.max(np.real((np.poly1d(sum_param) - y).roots))
     return inv_p(total_pe)
 
-
+'''
 
 
 def load_mc_event(pixel_values,pixel_list,master_fsm,param):
@@ -88,9 +89,9 @@ def load_mc_event(pixel_values,pixel_list,master_fsm,param):
     for patch in master_fsm.elements['cts_core'].cts.LED_patches:
         pe_in_pixels = []
         for pix in patch.leds_camera_pixel_id:
-            pe_in_pixels+=[pixel_values[pix]]
+            pe_in_pixels+=[pixel_values[pix]/4.]
         dac=get_patch_DAC(patch.leds_camera_pixel_id, pixel_list,pe_in_pixels, param)
-        master_fsm.elements['cts_core'].cts_client.set_ac_level(patch.camera_patch_id, dac)
+        master_fsm.elements['cts_core'].cts_client.set_ac_level(patch.camera_patch_id,int( round(dac)))
         pe = np.mean(pe_in_pixels)
         patch_dac.append(dac)
         patch_pe.append(pe)
@@ -100,7 +101,7 @@ def load_mc_event(pixel_values,pixel_list,master_fsm,param):
 def get_patch_DAC(pixels,pixel_list,pe_per_pixel,param):
     dac = 0
     total_pe = np.sum(pe_per_pixel)
-    if total_pe==0.: return 0.
+    if total_pe<0.5: return 0
     sum_param = np.zeros(param.shape[-1],dtype=param.dtype)
     for p in pixels:
         sum_param= sum_param+param[pixel_list.index(p)]
@@ -109,6 +110,7 @@ def get_patch_DAC(pixels,pixel_list,pe_per_pixel,param):
         dac = np.max(np.real((np.poly1d(sum_param) - total_pe).roots))
     except:
         dac = -1
+    if dac < 1e-3: dac == 0
     return int(np.round(dac))
 
 
@@ -122,8 +124,8 @@ def run(master_fsm):
     master_fsm.options['generator_configuration']['number_of_pulses'] =  \
         master_fsm.options['protocol_configuration']['events_per_mc_event']
 
-    master_fsm.options['writer_configuration']['max_evts_per_file'] = \
-        master_fsm.options['protocol_configuration']['events_per_mc_event']*10
+    if 'writer_configuration' in master_fsm.options.keys():
+        master_fsm.options['writer_configuration']['max_evts_per_file'] = master_fsm.options['protocol_configuration']['events_per_mc_event']*10
 
     # load the AC led calibration parameters
     ac_led_coefficient = np.load(master_fsm.options['protocol_configuration']['led_parameters_file'])['fit_result']
@@ -136,18 +138,12 @@ def run(master_fsm):
         return False
 
 
-
-    # Turn on the DC and AC LEDs
-    master_fsm.elements['cts_core'].all_on('DC',0)
-    master_fsm.elements['cts_core'].all_on('AC',0)
-
-
     dc_level = master_fsm.options['protocol_configuration']['dc_level']
     mc_events_file_path = master_fsm.options['protocol_configuration']['mc_events_file']
     N_mc_events = master_fsm.options['protocol_configuration']['N_mc_events']
 
     # load the mc events
-    mc_events = np.load(mc_events_file_path)
+    mc_events = np.load(mc_events_file_path)['mc_pes']
 
     pixel_list = []
     for pix in master_fsm.elements['cts_core'].cts.camera.Pixels:
@@ -155,13 +151,18 @@ def run(master_fsm):
             pixel_list.append(pix.ID)
 
     log.info('\033[1m\033[91m\t\t-|> Start the DAC level loop\033[0m' )
-    pbar = tqdm(total=N_mc_events)
+    pbar = tqdm(total=len(N_mc_events))
     tqdm_out = TqdmToLogger(log, level=logging.INFO)
 
-    for board in master_fsm.elements['cts_core'].cts.LED_boards:
-        master_fsm.elements['cts_core'].cts_client.set_dc_level(board.internal_id, dc_level )
+    
+    # Turn on the DC and AC LEDs
+    #master_fsm.elements['cts_core'].all_on('DC',0)
+    master_fsm.elements['cts_core'].all_on('AC',0)
 
-    for i in N_mc_events :
+    for board in master_fsm.elements['cts_core'].cts.LED_boards:
+        master_fsm.elements['cts_core'].cts_client.set_dc_level(board.internal_id, int(dc_level) )
+
+    for i in N_mc_events:
             load_mc_event(mc_events[i], pixel_list, master_fsm, param)
             timeout = master_fsm.options['protocol_configuration']['events_per_mc_event'] \
                       / master_fsm.options['generator_configuration']['rate']
@@ -172,8 +173,8 @@ def run(master_fsm):
             pbar.update(1)
 
     # Turn off the AC LEDs
-    master_fsm.elements['cts_core'].all_off('DC',0)
-    master_fsm.elements['cts_core'].all_off('AC', 0)
+    #master_fsm.elements['cts_core'].all_off('DC')
+    master_fsm.elements['cts_core'].all_off('AC')
 
     # And finalise the run
     if not end_run(master_fsm):
